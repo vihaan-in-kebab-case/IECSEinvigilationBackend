847785e5c8992d198f9842ded05acb79af7f0adf
VardaM0512 <varda.m512@gmail.com>
2025-12-22 13:46:43 +0530

    Update supabaseAdmin.js

--- utils/supabaseAdmin.js
+++ utils/supabaseAdmin.js
@@ -9,7 +9,7 @@ if (!process.env.SUPABASE_URL || !process.env.SUPABASE_KEY) {
 
 export const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY,
+  process.env.SUPABASE_SERVICE_ROLE_KEY,
   {
     auth: {
       persistSession: false,
