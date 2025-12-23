b4cdfab024a0d8f1602e596deb969def9cc10232
VardaM0512 <varda.m512@gmail.com>
2025-12-23 09:32:34 +0530

    Update supabaseAdmin.js

--- utils/supabaseAdmin.js
+++ utils/supabaseAdmin.js
@@ -3,13 +3,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-if (!process.env.SUPABASE_URL || !process.env.SUPABASE_SERVICE_ROLE_KEY) {
+if (!process.env.SUPABASE_URL || !process.env.SUPABASE_KEY) {
   throw new Error("Missing Supabase environment variables");
 }
 
 export const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY,
+  process.env.SUPABASE_KEY,
   {
     auth: {
       persistSession: false,
